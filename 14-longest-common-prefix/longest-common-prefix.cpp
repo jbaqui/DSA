8249025862bb8ee7db7d300311faf011b07dbae9
Josh Baquiano <86390557+jbaqui@users.noreply.github.com>
2026-02-18 09:45:43 +0800

    Time: 0 ms (100.00%) | Memory: 11.8 MB (82.39%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,16 +3,19 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty()) return "";
 
-        string prefix = strs[0];
+    for (int i = 0; i < strs[0].length(); i++) {
+        char currentChar = strs[0][i];
 
-        for (int i = 1; i < strs.size(); i++) {
-            while (strs[i].find(prefix) != 0) {
-                prefix = prefix.substr(0, prefix.length() - 1);
-                if (prefix.empty()) return "";
+        for (int j = 1; j < strs.size(); j++) {
+            // If index exceeds string length or chars differ
+            if (i >= strs[j].length() || strs[j][i] != currentChar) {
+                return strs[0].substr(0, i);
             }
         }
+    }
 
-        return prefix;
+    return strs[0];
+
     }
 
 };
